e2103f197877715abf059a9388456efe9221ff90
ume-meu <chi.nguyenthiyen.203@gmail.com>
2024-07-31 23:41:24 +0700

    create server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,54 @@
 require("dotenv").config();
 
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "*" }));
+// Enable CORS
+if (process.env.ENABLE_CORS === "true") {
+  app.use(cors({ origin: "*" }));
+}
 
 // Load JSON data from resources folder
-const register = JSON.parse(fs.readFileSync('./resources/Register.json', 'utf-8'));
-const opcode = JSON.parse(fs.readFileSync('./resources/Opcode.json', 'utf-8'));
+const register = JSON.parse(
+  fs.readFileSync("./resources/Register.json", "utf-8")
+);
+const opcode = JSON.parse(fs.readFileSync("./resources/Opcode.json", "utf-8"));
 
 app.get("/", async (req, res) => {
   res.json({ data: "hello" });
 });
 
 // API to get data by keyword
-app.get('/get-data/:keyword', async (req, res) => {
-    const keyword = req.params.keyword.toLowerCase();
-    const filteredData = register.filter(item => {
-        return (
-          item.name1.toLowerCase().includes(keyword) ||
-          item.name2.toLowerCase().includes(keyword) ||
-          item.register.toLowerCase().includes(keyword)
-        );
-    });
+app.get('/get-data', async (req, res) => {
+  // Retrieve and decode the keyword
+  const keyword = req.query.keyword;
 
-    res.json(filteredData);
+  console.log(keyword);
+  
+  // Split the keyword into an array based on spaces
+  const keywordArray = keyword.split(/\s+/);
+
+  // Log the array to verify
+  console.log('Keyword Array:', keywordArray);
+
+  // Perform filtering based on the split array
+  const filteredData = register.filter(item => {
+      return keywordArray.some(word =>
+          item.name1.toLowerCase().includes(word.toLowerCase()) ||
+          item.name2.toLowerCase().includes(word.toLowerCase())
+      );
+  });
+
+  res.json(filteredData);
+  // Prevent caching
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
 });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port);
 console.log(`Running on http://localhost:${port}`);
--- frontend/converter/src/components/SearchBar.jsx
+++ frontend/converter/src/components/SearchBar.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPaw } from "react-icons/fa6";
+import axios from "axios";
 
-const SearchBar = ({inputWidth}) => {
+const SearchBar = ({ inputWidth }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Search for MIPS instruction
+  const onSearch = async (e) => {
+    e.preventDefault();
+  
+    // Add a random parameter to prevent caching
+    const timestamp = new Date().getTime();
+    const url = `/get-data?keyword=${searchTerm}&t=${timestamp}`; 
+  
+    try {
+      const res = await axios.get(url, { cache: false });
+      console.log(res.data);
+    } catch (error) {
+      console.log("Error fetching data: ", error);
+    }
+  };
+
   return (
     <div
       className="m-auto bg-[white] h-fit flex items-center border-[#955ed3] justify-between px-2.5 py-[5px] rounded-[40px] border-[2.5px] border-solid"
-        style={{ width: `${inputWidth}%` }}
+      style={{ width: `${inputWidth}%` }}
     >
       <input
         type="text"
@@ -14,9 +33,11 @@ const SearchBar = ({inputWidth}) => {
         id="search-input"
         name="mips="
         required
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
 
-      <button type="submit" className="search-btn">
+      <button type="submit" className="search-btn" onClick={onSearch}>
         <FaPaw />
       </button>
     </div>
